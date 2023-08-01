60ddc8efd3dffb29725bd5d3ea0009693a75676f
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-08-01 16:02:40 +0000

    simplify

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,10 +8,10 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (!node || !node->parent || !(node->parent->left && node->parent->right))
+	if (!node || !node->parent)
 		return (NULL);
 
-	if (node->parent->left->n != node->n)
+	if (node->parent->left != node)
 		return (node->parent->left);
 
 	return (node->parent->right);
