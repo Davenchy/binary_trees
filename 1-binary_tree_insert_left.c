75e9d9ed790786313c88e086742c134d3f8719a0
Fadi Asaad <firon1222@gmail.com>
2023-08-01 05:42:14 +0300

    update task 1: check if parent is NULL before memory allocation to prevent memory leak if parent is NULL

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,14 +11,14 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
+	/* parent is required to insert nodes */
+	if (!parent)
+		return (NULL);
+
 	/*allocate memory for a new node*/
 	new_node = malloc(sizeof(binary_tree_t));
-
-	if (!new_node || !parent)
-	{
-		/*malloc failed or parent is NULL*/
+	if (!new_node)
 		return (NULL);
-	}
 
 	new_node->n = value;
 	new_node->parent = parent;
