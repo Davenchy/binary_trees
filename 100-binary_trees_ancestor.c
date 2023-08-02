b10e3c6ee5f66d11f3b35689d59371b116f014d5
Fadi Asaad <firon1222@gmail.com>
2023-08-02 09:23:34 +0300

    update task 100: reimplement the algorithm

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,6 +1,21 @@
 #include "binary_trees.h"
 
 /**
+ * has_node - Check if a sub-tree contains a node
+ * @root: Pointer to the root node of the sub-tree
+ * @node: Pointer to the node to check
+ * Return: 1 if the node is in the sub-tree and 0 otherwise
+ */
+int has_node(binary_tree_t *root, binary_tree_t *node)
+{
+	if (root == NULL)
+		return (0);
+	if (root == node)
+		return (1);
+	return (has_node(root->right, node) || has_node(root->left, node));
+}
+
+/**
  * binary_trees_ancestor - finds the ancestor of a node
  * @first: pointer to the first node
  * @second: pointer to the second node
@@ -9,25 +24,16 @@
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 																		 const binary_tree_t *second)
 {
+	binary_tree_t *min = NULL, *max = NULL;
+
 	if (!first || !second)
 		return (NULL);
-	else if (first->n == second->n)
-		return ((binary_tree_t *) first);
 
-	if (first->n > second->n)
-	{
-		if (second->parent)
-			return (binary_trees_ancestor(first, second->parent));
-		else
-			return ((binary_tree_t *) second);
-	}
-	else if (first->n < second->n)
-	{
-		if (first->parent)
-			return (binary_trees_ancestor(first->parent, second));
-		else
-			return ((binary_tree_t *) first);
-	}
+	max = (binary_tree_t *)(first->n > second->n ? first : second);
+	min = (binary_tree_t *)(first->n > second->n ? second : first);
 
-	return ((binary_tree_t *) first);
+	while (min && min->parent && !has_node(min->parent, max))
+		min = min->parent;
+
+	return ((binary_tree_t *)min->parent);
 }
