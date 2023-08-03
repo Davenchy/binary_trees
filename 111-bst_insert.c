33020cd4a1eed000dc86a3b2f2152109cac0203b
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-08-03 12:28:49 +0000

    fix style

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,27 +1,31 @@
 #include "binary_trees.h"
 
+/**
+ * insert_leaf - inserts a leaf in a binary search tree
+ * @tree: is a pointer to the root node of the BST to insert in
+ * @value: is the value to insert in the tree
+ *
+ * Return: a pointer to the created node, or NULL on failure
+ */
 bst_t *insert_leaf(bst_t **tree, int value)
 {
 	if (value < (*tree)->n)
 	{
 		if ((*tree)->left)
 			return (insert_leaf(&((*tree)->left), value));
-		else
-		{
-			(*tree)->left = binary_tree_node(*tree, value);
-			return ((*tree)->left);
-		}
+
+		(*tree)->left = binary_tree_node(*tree, value);
+		return ((*tree)->left);
+
 	}
 
 	if (value > (*tree)->n)
 	{
 		if ((*tree)->right)
 			return (insert_leaf(&((*tree)->right), value));
-		else
-		{
-			(*tree)->right = binary_tree_node(*tree, value);
-			return ((*tree)->right);
-		}
+
+		(*tree)->right = binary_tree_node(*tree, value);
+		return ((*tree)->right);
 	}
 
 	/*then value already exits -> ignore it*/
